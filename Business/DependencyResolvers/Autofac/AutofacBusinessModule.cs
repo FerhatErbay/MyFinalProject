3119f6541cf612a9fc0bf705c360fbcdb4df4721
AmorFatiBA <you@example.com>
2021-02-24 00:14:18 +0200

    23.02.2021 Commit

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,36 +1,12 @@
 ﻿using System;
 using Autofac;
-using Autofac.Extras.DynamicProxy;
-using Business.Abstract;
-using Business.Concrete;
-using Castle.DynamicProxy;
-using Core.Utilities.Interceptors;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
 
 namespace Business.DependencyResolvers.Autofac
 {
     public class AutofacBusinessModule : Module
     {
-        public AutofacBusinessModule()
-        {
-        }
+        
 
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
-            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
-
-
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
-                {
-                    Selector = new AspectInterceptorSelector()
-                }).SingleInstance();
-
-
-        }
+        
     }
 }
